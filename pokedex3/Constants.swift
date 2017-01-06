ce9cfe7798bf9fe7c4da77451fd144e2a42f7925
Clyff Millet <clyffm@gmail.com>
2017-01-06 17:04:22 -0500

    with alamo fire

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Clyfford Millet on 1/5/17.
+//  Copyright © 2017 Clyff Millet. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
